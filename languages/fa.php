2d86cdfd971db05d6ad29c7f274978163dd5f12a
Mohammad Shiran <vbadvanced@gmail.com>
2023-05-01 16:31:07 +0330

    Added Persian(fa) translation

---
+++ languages/fa.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'menu.link.add' => 'افزودن',
+    'menu.link.title' => 'پیوند صفحه',
+    'menu.link.text' => 'افزودن پیوند صفحات داخلی وبسایت',
+    'menu.custom.title' => 'پیوند دلخواه',
+    'menu.custom.text' => 'افزودن پیوند دلخواه',
+    'editor.label.id' => 'شناسه',
+    'editor.label.url' => 'آدرس',
+    'editor.label.text' => 'متن',
+    'editor.label.title' => 'عنوان',
+    'editor.label.popup' => 'باز شدن در تب جدید؟',
+    'editor.menu.close' => 'بستن',
+    'editor.menu.edit' => 'ویرایش',
+    'editor.menu.remove' => 'حذف',
+    'editor.menu.duplicate' => 'تکثیر',
+    'modal.link.title' => 'افزودن صفحه',
+    'modal.link.breadcrumb' => 'بازگشت',
+    'modal.custom.title' => 'افزودن لینک دلخواه',
+    'help.depth.text' => 'حداکثر عمق مجاز منوها:',
+    'help.empty.text' => 'آیتمی اضافه نشده است'
+];
